d78dbe6fab3959eda1a06869740dc0cfbacb873a
YugenFox <kalebsday@gmail.com>
2021-09-14 22:13:04 -0500

    commit to sept 14th, past

--- 2022-10/Sort array by string length.js
+++ 2022-10/Sort array by string length.js
@@ -1,6 +1,6 @@
 //https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/javascript
 
-//my own
+//my own test
 function sortByLength (array) {
   // Return an array containing the same strings, ordered from shortest to longest
   var sortedArr = array.sort((a,b) => a.length - b.length)
