23ead3e6d427b425eb8dd055e577b692efbaaf49
YugenFox <kalebsday@gmail.com>
2022-09-23 23:02:34 -0500

    update

---
+++ 2022-10/Counting sheep...js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    totalSheep = 0
+    for(let i = 0; i < arrayOfSheep.length; i++){
+      if(arrayOfSheep[i] === true){
+        totalSheep++;  
+      }
+      
+    }
+    
+    return totalSheep
+  }
