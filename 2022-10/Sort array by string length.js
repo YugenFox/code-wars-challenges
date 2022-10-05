3f5880a08ca2ddb56e1d9823e813cde3fa674169
YugenFox <kalebsday@gmail.com>
2022-10-04 19:01:14 -0500

    Adding recent array code wars

---
+++ 2022-10/Sort array by string length.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/javascript
+
+function sortByLength (array) {
+  // Return an array containing the same strings, ordered from shortest to longest
+  var sortedArr = array.sort((a,b) => a.length - b.length)
+
+  return sortedArr
+
+};
