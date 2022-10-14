c834d1dc4287ace9d7e2a02ca75bc214632dc216
YugenFox <kalebsday@gmail.com>
2022-10-14 00:14:39 -0500

    extract domain name from a URL

---
+++ 2022-10/Extract the domain name from a URL.js
@@ -0,0 +1,23 @@
+//https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
+
+let test1 = "http://github.com/carbonfive/raygun"
+let test2 = "http://www.bkj8ycolz9857s70g.us/error"
+function domainName(url){
+    retArr = []
+    //remove http://
+    url = url.replace("http://", "")
+    //remove https://
+    url = url.replace("https://", "")
+    //remove www.
+    url = url.replace("www.", "")
+    //split .
+    retArr = url.split(".")
+      //choose index 0 for domain
+    
+    return retArr[0]
+  }
+  
+  console.log(domainName(test2))
+  //"http://github.com/carbonfive/raygun" => carbonfive
+  //"http://www.zombie-bites.com" =>  zombie-bites
+  //"https://www.cnet.com" => cnet
