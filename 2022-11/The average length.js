c4331da66f923994767c2e4c4133a67978c1b194
YugenFox <kalebsday@gmail.com>
2022-11-16 13:22:13 -0600

    The average length - refactored

--- 2022-11/The average length.js
+++ 2022-11/The average length.js
@@ -1,6 +1,30 @@
 //https://www.codewars.com/kata/5a430359e1ce0e35540000b1/train/javascript
 
 function averageLength(arr) { 
+    ///new array that takes arr indexes and turns them into numbers based on index length
+    const numMap = arr.map(e => e.length)
+    
+    //reduce numMap array to find total indexes string length
+    const sumTotalLength = numMap.reduce(
+      (prevVal, curVal) => prevVal + curVal, 0
+    )
+    
+    /// divide that number by arr.length to find avgLength
+    const avgLength = Math.round(sumTotalLength / numMap.length)
+
+    //rewrite OG array to have each index with the same amount of chars as avgLength
+    const avgLengthReturn = arr.map(e => e[0].repeat(avgLength))
+
+    return avgLengthReturn
+}
+
+//test
+console.log(averageLength(['u', 'y']), ['u', 'y']) 
+console.log(averageLength(['aa', 'bbb', 'cccc']), ['aaa', 'bbb', 'ccc']) 
+
+//not refactored
+/*
+function averageLength(arr) { 
     ///make arr changing strings to numbers based on string length
     const numMap = arr.map(e => e.length)
     console.log(numMap, "NumMap")
@@ -19,7 +43,4 @@ function averageLength(arr) {
 
     return avgLengthReturn
 }
-
-//test
-console.log(averageLength(['u', 'y']), ['u', 'y']) 
-console.log(averageLength(['aa', 'bbb', 'cccc']), ['aaa', 'bbb', 'ccc']) 
+*/
