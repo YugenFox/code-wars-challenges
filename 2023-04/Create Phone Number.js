9055ed2c801c42831f0cd4ec85753fa12d5068f3
YugenFox <kalebsday@gmail.com>
2023-04-18 11:00:04 -0500

    Phone number kata from array done

---
+++ 2023-04/Create Phone Number.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+//mine
+function createPhoneNumber(numbers) {
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+}
+
+//cleaner idea
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
